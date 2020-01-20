f1416ce74fca1ef49e2c1c1090359680ab294b3c
krmax44 <hi@krmax44.de>
2020-01-20 21:41:54 +0100

    fix: collect coverage from vue

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	...require('@vue/cli-plugin-unit-jest/presets/typescript/jest-preset'),
 	transformIgnorePatterns: ['node_modules/(?!(vue-material-design-icons)/)'],
-	coverageReporters: ['text', 'lcovonly']
+	coverageReporters: ['text', 'lcovonly'],
+	collectCoverageFrom: ['./src/**/*.{js,ts,vue}', '!**/node_modules/**']
 };
