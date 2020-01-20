95bdbe75354d6cba4dd4cb507a2765dfafc6809a
krmax44 <hi@krmax44.de>
2020-01-20 20:10:08 +0100

    chore(ci): add codecov

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
 	...require('@vue/cli-plugin-unit-jest/presets/typescript/jest-preset'),
-	transformIgnorePatterns: ['node_modules/(?!(vue-material-design-icons)/)']
+	transformIgnorePatterns: ['node_modules/(?!(vue-material-design-icons)/)'],
+	coverageReporters: ['text', 'lcovonly']
 };
