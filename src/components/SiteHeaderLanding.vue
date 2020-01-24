898411644ff6acef1f72d5b119e0c79a577b8844
krmax44 <hi@krmax44.de>
2020-01-24 16:50:18 +0100

    fix: landing header

--- src/components/SiteHeaderLanding.vue
+++ src/components/SiteHeaderLanding.vue
@@ -1,7 +1,7 @@
-<template>
+<template functional>
 	<header>
 		<img src="../assets/images/logo.svg" class="logo" alt="clef.ninja Logo" />
-		<h1 class="brand">
+		<h1 class="brand opacity-100">
 			<span class="font-bold">clef</span>
 			<span class="text-gray-800">.ninja</span>
 		</h1>
