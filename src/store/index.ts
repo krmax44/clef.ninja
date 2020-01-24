283ea48b4cc80fc108c4b3afe6de7148bd47dc71
krmax44 <hi@krmax44.de>
2020-01-24 19:32:26 +0100

    feat: persistant settings

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,22 +1,42 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
 import InstrumentPiano from '@/instruments/InstrumentPiano';
 import InstrumentGuitar from '@/instruments/InstrumentGuitar';
+import MidiHandler from '@/utils/MidiHander';
+import Instrument from '@/instruments/Instrument';
 
 Vue.use(Vuex);
+
+const vuexLocal = new VuexPersistence<State>({
+	reducer: ({ keyboard, instrument }) => ({ keyboard, instrument })
+});
 
 const instruments = {
 	piano: InstrumentPiano,
 	guitar: InstrumentGuitar
 };
 
-export default new Vuex.Store({
+interface State {
+	stage: {
+		name: 'homeView' | 'settingsView' | 'gamePlay';
+		transition: 'forwards' | 'backwards';
+	};
+	gamemode?: 'training' | 'arcade';
+	keyboard: {
+		type: 'virtual' | 'midi';
+		midi?: MidiHandler;
+	};
+	instrument: Instrument;
+}
+
+export default new Vuex.Store<State>({
 	state: {
 		stage: {
 			name: 'homeView',
 			transition: 'forwards'
 		},
-		gamemode: '',
+		gamemode: undefined,
 		keyboard: {
 			type: 'virtual',
 			midi: undefined
@@ -47,5 +67,6 @@ export default new Vuex.Store({
 		midi(state) {
 			return state.keyboard.type === 'midi' && state.keyboard.midi;
 		}
-	}
+	},
+	plugins: [vuexLocal.plugin]
 });
