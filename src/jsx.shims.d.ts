1f72b650db01b8bec7aa0e9da1ebac81b6b7a599
krmax44 <hi@krmax44.de>
2020-02-13 21:15:30 +0100

    fix: seperate shims

---
+++ src/jsx.shims.d.ts
@@ -0,0 +1,13 @@
+import Vue, { VNode } from 'vue';
+declare global {
+	namespace JSX {
+		interface Element extends VNode {}
+		interface ElementClass extends Vue {}
+		interface ElementAttributesProperty {
+			$props: {};
+		}
+		interface IntrinsicElements {
+			[elemName: string]: any;
+		}
+	}
+}
--- src/vue.shims.d.ts
+++ src/vue.shims.d.ts
@@ -2,17 +2,3 @@ declare module '*.vue' {
 	import Vue from 'vue';
 	export default Vue;
 }
-
-import Vue, { VNode } from 'vue';
-declare global {
-	namespace JSX {
-		interface Element extends VNode {}
-		interface ElementClass extends Vue {}
-		interface ElementAttributesProperty {
-			$props: {};
-		}
-		interface IntrinsicElements {
-			[elemName: string]: any;
-		}
-	}
-}
