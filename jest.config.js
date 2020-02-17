3254d720bd6498ed6bc904d5b78f3646490c1044
Max <hi@krmax44.de>
2020-02-17 21:54:33 +0100

    fix: collect coverage from tsx files

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
 	...require('@vue/cli-plugin-unit-jest/presets/typescript-and-babel/jest-preset'),
 	transformIgnorePatterns: ['node_modules/(?!(vue-material-design-icons)/)'],
 	coverageReporters: ['text', 'lcovonly'],
-	collectCoverageFrom: ['./src/**/*.{js,ts,vue}', '!**/node_modules/**'],
+	collectCoverageFrom: ['./src/**/*.{js,ts,tsx,vue}', '!**/node_modules/**'],
 	setupFiles: ['./src/testUtils/window.ts']
 };
