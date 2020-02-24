7094765d2cdc0843cf727bd33c9d03735154f797
krmax44 <hi@krmax44.de>
2020-02-24 19:51:04 +0100

    fix: tidy up

--- src/utils/randomHelper.ts
+++ src/utils/randomHelper.ts
@@ -1,7 +1,3 @@
 export function randomFromArray<T>(array: T[]) {
 	return array[Math.floor(Math.random() * array.length)];
 }
-
-export function randomBetween(MIN: number, MAX: number) {
-	return Math.floor(Math.random() * (MAX - MIN)) + MIN;
-}
